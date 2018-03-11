104330e25ba674b1e07fedaa7bd20f3ad59c31c3
Josip Cavar <josipcavar04@gmail.com>
2018-03-11 22:17:51 +0100

    Update rebuild script to take current dir into account

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,6 @@
-mustache data.yml index.mustache > index.html; 
-mustache data.yml about.mustache > about.html; 
-mustache data.yml contact.mustache > contact.html;
-sass --update scss:public/;
+
+directory="${0%/*}";
+mustache "$directory"/data.yml "$directory"/index.mustache > "$directory"/index.html; 
+mustache "$directory"/data.yml "$directory"/about.mustache > "$directory"/about.html; 
+mustache "$directory"/data.yml "$directory"/contact.mustache > "$directory"/contact.html;
+sass --update --default-encoding=UTF-8 "$directory"/scss:"$directory"/public/;
